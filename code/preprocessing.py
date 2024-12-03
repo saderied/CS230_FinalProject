65cbcfaa305179be600e05891f0d6f1b99869ec6
cjanowiak <66145646+cjanowiak@users.noreply.github.com>
2024-12-03 09:36:57 -0800

    Added changes to data preprocessing

--- code/preprocessing.py
+++ code/preprocessing.py
@@ -2,21 +2,22 @@ import pandas as pd
 import numpy as np
 
 csv_path = "/Users/connorjanowiak/Documents/Stanford/CS230/data/itineraries.csv"  
-output_parquet_path = "/Users/connorjanowiak/Documents/Stanford/CS230/data/itineraries.parquet"
+output_csv_path = "/Users/connorjanowiak/Documents/Stanford/CS230/data/processed_itineraries.csv"
 
 def preprocess_flight_data(csv_path, output_parquet_path, chunksize=100000):
     columns_to_keep = [
         "legId", "searchDate", "flightDate", "startingAirport", 
         "destinationAirport", "travelDuration", "isBasicEconomy", 
         "isRefundable", "isNonStop", "baseFare", "seatsRemaining", 
-        "totalTravelDistance"
+        "totalTravelDistance", "segmentsAirlineName", "segmentsCabinCode",
+        "segmentsDepartureAirportCode"
     ]
     
     processed_data = []
     cur_chunk = 0
     for chunk in pd.read_csv(csv_path, chunksize=chunksize):
         cur_chunk += 1
-        print("Processing chunk: " + str(cur_chunk))
+        print("Processing chunk: " + str(cur_chunk), end='\r')
         chunk = chunk[columns_to_keep]
         chunk['travelDuration'] = chunk['travelDuration'].str.extract(r'PT(\d+)H(\d+)M').fillna(0).astype(int).apply(
             lambda x: x[0] * 60 + x[1], axis=1)
@@ -26,14 +27,15 @@ def preprocess_flight_data(csv_path, output_parquet_path, chunksize=100000):
         chunk['daysElapsed'] = (chunk['flightDate'] - chunk['searchDate']).dt.days
         
         chunk['isWeekend'] = chunk['flightDate'].dt.dayofweek.isin([5, 6])
-        
-        
-        categorical_cols = ['startingAirport', 'destinationAirport']
+        chunk['totalStops'] = chunk['segmentsDepartureAirportCode'].str.count(r'\|\|')
+        chunk['airline'] = chunk['segmentsAirlineName'].str.split('||').str[0]
+        chunk['cabinCode'] = chunk['segmentsCabinCode'].str.split('||').str[0]
+        categorical_cols = ['startingAirport', 'destinationAirport', 'airline', 'cabinCode']
         chunk = pd.get_dummies(chunk, columns=categorical_cols, drop_first=True)
-        
+        chunk.drop(["segmentsAirlineName", "segmentsCabinCode", "segmentsDepartureAirportCode"], axis=1, inplace=True)
         processed_data.append(chunk)
     
     final_df = pd.concat(processed_data, ignore_index=True)
-    final_df.to_parquet(output_parquet_path, compression='snappy')
+    final_df.to_csv(output_parquet_path)
 
-preprocess_flight_data(csv_path, output_parquet_path)
+preprocess_flight_data(csv_path, output_csv_path)
--- code/preprocessing_temporal.py
+++ code/preprocessing_temporal.py
@@ -4,17 +4,18 @@ import h5py
 
 pd.set_option('future.no_silent_downcasting', True)
 
+chunksize = 10_000_000
+window_size = 14
 
-data_path = "/Users/connorjanowiak/Documents/Stanford/CS230/data/itineraries.parquet"
-data = pd.read_parquet(data_path)
-data = data.head(20_000_000)
-data = data.dropna()
-data.replace({False: 0, True: 1}, inplace=True)
-
-data = data.sort_values(by=['legId', 'searchDate'])
-
-data['day_of_week'] = data['searchDate'].dt.dayofweek
-data['week_of_year'] = data['searchDate'].dt.isocalendar().week
+data_path = "/Users/connorjanowiak/Documents/Stanford/CS230/data/processed_itineraries.csv"
+output_path = "/Users/connorjanowiak/Documents/Stanford/CS230/data/temporal_data.h5"
+data_sample = pd.read_csv(data_path, nrows=10)
+feature_cols = data_sample.columns.to_list()
+feature_cols.remove('baseFare')
+feature_cols.remove('legId')
+feature_cols.remove('flightDate')
+feature_cols.remove('searchDate')
+target_col = 'baseFare'
 
 
 def create_sequences(data, group_col, feature_cols, target_col, window_size):
@@ -26,14 +27,30 @@ def create_sequences(data, group_col, feature_cols, target_col, window_size):
             y.append(group_values[i+window_size, -1])
     return np.array(X, dtype=np.float32), np.array(y, dtype=np.float32)
 
-feature_cols = data.columns.to_list()
-feature_cols.remove('baseFare')
-feature_cols.remove('legId')
-feature_cols.remove('flightDate')
-feature_cols.remove('searchDate')
-target_col = 'baseFare'
-window_size = 14
-X, y = create_sequences(data, group_col='legId', feature_cols=feature_cols, target_col=target_col, window_size=window_size)
-with h5py.File('/Users/connorjanowiak/Documents/Stanford/CS230/data/temporal_data.h5', 'w') as hf:
-    hf.create_dataset('X', data=X, compression='gzip')
-    hf.create_dataset('y', data=y, compression='gzip')
+
+with h5py.File(output_path, 'w') as hf:
+    hf.create_dataset('X', shape=(0, window_size, len(feature_cols)), maxshape=(None, window_size, len(feature_cols)), dtype=np.float32, compression='gzip')
+    hf.create_dataset('y', shape=(0,), maxshape=(None,), dtype=np.float32, compression='gzip')
+
+for chunk_index, chunk in enumerate(pd.read_csv(data_path, chunksize=chunksize)):
+    print(f"Processing chunk {chunk_index + 1}", end='\r')
+    
+    # This is needed to account for the last chunk being smaller
+    if chunk_index == 7:
+        break
+    
+    chunk.dropna(inplace=True)
+    chunk.replace({False: 0, True: 1}, inplace=True)
+    chunk['searchDate'] = pd.to_datetime(chunk['searchDate'])
+    chunk = chunk.sort_values(by=['legId', 'searchDate'])
+    chunk['day_of_week'] = chunk['searchDate'].dt.dayofweek
+    chunk['week_of_year'] = chunk['searchDate'].dt.isocalendar().week
+
+    X_chunk, y_chunk = create_sequences(
+        chunk, group_col='legId', feature_cols=feature_cols, target_col=target_col, window_size=window_size
+    )
+    with h5py.File(output_path, 'a') as hf:
+        hf['X'].resize(hf['X'].shape[0] + X_chunk.shape[0], axis=0)
+        hf['X'][-X_chunk.shape[0]:] = X_chunk
+        hf['y'].resize(hf['y'].shape[0] + y_chunk.shape[0], axis=0)
+        hf['y'][-y_chunk.shape[0]:] = y_chunk
